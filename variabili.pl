1914a9ce6fab5498d5d1b7a0f626c29c63ad13ca
ADM <aaacoadm@gmail.com>
2026-04-05 12:46:23 +0200

    esercizio_tipi

---
+++ variabili.pl
@@ -0,0 +1,21 @@
+use strict;
+use warnings;
+
+
+#Esercizio variabili
+#variabili $
+my $nome = "Lukas";
+my $eta = 255;
+print "Mi chiamo $nome e ho $eta anni \n ";
+
+#variabili @
+my @numeri = (1,2,3,4,5);
+print "questo e' il primo num: $numeri[0] \n ";
+
+#variabili %
+my %person = (
+    nome => "luskass",
+    eta => 390
+  );
+print "ciao $person{nome} hai $person{eta} \n ";
+
