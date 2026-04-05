04092ab1a338647a62cab48c17a7ef4fd91c7fd5
ADM <aaacoadm@gmail.com>
2026-04-05 11:59:18 +0200

    Add files via upload

---
+++ conta.pl
@@ -0,0 +1,12 @@
+#esercizio ciclo while
+
+use strict;
+use warnings;
+
+my $contatore = 0;
+
+while ($contatore <= 10) {
+  print "sto contando : $contatore \n ";
+  $contatore += 1;
+}
+print "finito di contare ... \n ";
